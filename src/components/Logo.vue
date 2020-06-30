0b33ad36251874d94b6d27f446d51f8ec855890a
Alex <MKmoolooo@yandex.ru>
2020-06-30 13:10:18 +0400

    Logo added

---
+++ src/components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 class="main">Jojoba</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
+
+.main{
+    position: absolute;
+    bottom:0%;
+    right:5%;
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 96px;
+    line-height: 112px;
+    /* Text color */
+    color: #000000;
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import three from 'three'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  three,
   router,
   render: h => h(App)
 }).$mount('#app')
